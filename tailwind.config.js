e5050b0ec5b743f32cffcbaa4731f20b4525b524
Anh Quan Nguyen <quan.na4102001@gmail.com>
2024-05-15 15:05:45 +0700

    Responsive header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        Karla: ["Karla", "sans-serif"],
+      },
       colors: {
         "light-coffee": "#C89F94",
+      },
+      keyframes: {
+        slideDown: {
+          "100%": { transform: "translateY(0)" },
+          "0%": { transform: "translateY(-100%)" },
+        },
+      },
+      animation: {
+        slideDown: "slideDown .4s ease-in-out",
       },
     },
   },
